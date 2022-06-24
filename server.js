8fd8daa030069728aaad4c8c0a7be074e74f73d5
Tran Duc Minh <tranfminh@gmail.com>
2022-06-24 08:55:13 +0700

    log request

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ const config = require('./config');
 const connection = require('./src/db/connection');
 const { requestHandler } = require('./src/middlewares/RequestHandler');
 const { Logger } = require('common');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const stream = {
+  write: (message) => Logger.info(message),
+};
+
+const skip = () => {
+  const env = process.env.NODE_ENV || 'development';
+  return env !== 'development';
+};
 
 async function initServer() {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(cors(config.cors));
+  app.use(morgan('dev', { stream: stream, skip: skip }));
   app.use(device.capture());
   app.use(requestHandler);
   await connection.init();
